098e1eeb3a8d0a4a8b6c7747f8f6b25dbdb0ea1b
BrandonPessman <pessman.brandon@gmail.com>
2022-04-20 00:52:34 -0500

    chore: remove no-console logs

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,11 @@ const app = express()
 const port: number = 8000
 
 app.get('/ping', (req, res) => {
-    res.status(200).json({ data: 'pong' })
+	res.status(200).json({ data: 'pong' })
 })
 
 app.listen(port, () => {
-    // tslint:disable-next-line:no-console
-    console.log(`server started at http://localhost:${port}`)
+	console.log(`server started at http://localhost:${port}`)
 })
 
 module.exports = app
